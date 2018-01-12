21638d4051dd921c4eeabe4571a68752b17bed60
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-01-12 21:11:48 +0530

    Add files via upload
    WAP to accpet n numbers from user and display their addition

---
+++ basic/variable arguments/var_Arg_add.py
@@ -0,0 +1,18 @@
+'''
+WAP to accept n numbers from user and display their addition.
+Note: use variable number of arguments
+'''
+def add(*iNum):
+	iSum = 0
+	for iTemp in iNum:
+		iSum += iTemp
+	return iSum
+
+def main():
+	print("Addition of two numbers:{}".format(add(2,3)))
+	print("Addition of three numbers:{}".format(add(2,5,5)))
+	print("Addition of four numbers:{}".format(add(2,3,4,5)))
+	print("Addition of five numbers:{}".format(add(2,3,4,5,6)))
+
+if __name__ == '__main__':
+	main()
