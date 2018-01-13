759f248e757ea6f48a2effdace41e94fb9ec3bee
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-01-13 11:17:16 +0530

    Update factorial.py

--- basic/control flow/factorial.py
+++ basic/control flow/factorial.py
@@ -3,16 +3,24 @@
 WAP to accept a number form user and display its factorial
 
 '''
-
+#non-recursive function
 def factorial(iNum):
 	iFact = 1
+	iNum = abs(iNum)	#for negative numbers, convert to positive numbers
 	for iTemp in range(2, iNum + 1):
 		iFact *= iTemp
 	return iFact
 	
+#direct-recursive function
+def xfactorial(iNum):
+	if (2 > iNum):
+		return 1
+	return iNum * xfactorial(iNum-1)
+
 def main():
 	iNum = input("Enter number:")
 	print("Fatorial is :", factorial(iNum))
+	print("Fatorial is :", xfactorial(iNum))
 
 if __name__ == '__main__':
 	main()
