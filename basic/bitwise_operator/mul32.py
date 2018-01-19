bd845e6ffdccac9670672786fb6df4e99e990ba6
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-01-19 21:47:58 +0530

    Update mul32.py

--- basic/bitwise_operator/mul32.py
+++ basic/bitwise_operator/mul32.py
@@ -1,12 +1,9 @@
 def Mul32(iNum):
-	bFlag = False
-	if iNum & 15 == 0:
-		bFlag = True
-	return bFlag
+	return iNum & 15
 	
 def main():
 	iNum = input("Enter number:")
-	if (Mul32(iNum)):
+	if (0 == Mul32(iNum)):
 		print("{} is a multiple of 32".format(iNum))
 	else:
 		print("{} is NOT multiple of 32".format(iNum))
