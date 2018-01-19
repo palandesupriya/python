6b796b42434d68d4e4ce26f61e30b0746bd3da0f
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-01-19 15:22:33 +0530

    Update cntbits.py

--- basic/bitwise_operator/cntbits.py
+++ basic/bitwise_operator/cntbits.py
@@ -1,11 +1,20 @@
 '''
 
-WAP to count number of bits ON in a postive number
+WAP to count number of bits ON in a postive number and negative number
 
 '''
-def CountOneBits(no):
+def CountOneBitsNeg(no):
 	x = 1 
 	count = 0 
+	while x <= max(2**32 - 1, 2**64 - 1):
+		if no & x !=0:
+			count += 1
+		x = x << 1
+	return count
+	
+def CountOneBitsPos(no):
+	x = 1 
+	count = 0 	
 	while x <= no:
 		if no & x !=0:
 			count += 1
@@ -14,7 +23,10 @@ def CountOneBits(no):
 	
 def main():
 	iNum = input("Enter a number:")
-	print("Number of bits On in {} are {}".format(iNum, CountOneBits(iNum)))
+	if (iNum < 0):
+		print("Number of bits On in {} are {}".format(iNum, CountOneBitsNeg(iNum)))
+	else:
+		print("Number of bits On in {} are {}".format(iNum, CountOneBitsPos(iNum)))
 	
 if __name__ == '__main__':
 	main()
