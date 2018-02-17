025aad1cf833a2813029b674e31a4a62e793b029
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-02-17 19:52:35 +0530

    Update altrchar.py

--- file handling/altrchar.py
+++ file handling/altrchar.py
@@ -1,19 +1,30 @@
 '''
 WAP to accept file name and display alternate n(or config this) characters.(use seek)
 '''
+
 def readalter(szfnm, iNum):
 	fp = open(szfnm, "r")
-	ch = fp.read(1)
-	szAlter = ""
-	while(ch != ""):
-		szAlter += ch
+	chFirstAltr = ""
+	chSecondAltr = ""
+	chFirstAltr = fp.read(iNum)
+	while(chFirstAltr != ""):
+		print chFirstAltr
 		fp.seek(iNum, 1)
-		ch = fp.read(1)
-	print szAlter
+		chFirstAltr = fp.read(iNum)
+
+	print("****************************************************")
+	fp.seek(iNum, 0)
+	chFirstAltr = fp.read(iNum)
+	while(chFirstAltr != ""):
+		print chFirstAltr
+		fp.seek(iNum, 1)
+		chFirstAltr = fp.read(iNum)
+
 def main():
-	#szfnm = input("Enter a file name:")
-	szfnm = "hw.txt"
+	szfnm = input("Enter a file name:")
+	#szfnm = "hw.txt"
 	iNum = input("Enter characters to skip:")
 	readalter(szfnm, iNum)
+
 if __name__ == '__main__':
 	main()
