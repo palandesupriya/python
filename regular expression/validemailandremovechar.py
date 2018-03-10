f15cf64f21b26ad83c02c0549d3b29ac9fcab554
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-03-10 19:05:12 +0530

    Create validemailandremovechar.py

---
+++ regular expression/validemailandremovechar.py
@@ -0,0 +1,33 @@
+'''
+1)WAP to validate E-mail ID using regular expressions
+2)WAP to accept an alphanumeric string from user and remove all characters other than digits
+	using regular expressions
+'''
+import re
+def verifyemail(szEmail):
+	regExpr = re.search("[a-zA-Z0-9]+\@{1}[a-zA-Z]+\.{1}[a-zA-Z]+", szEmail)
+	if None == regExpr:
+		return False
+	if len(szEmail) == regExpr.end():
+		return True
+	return False
+
+def rmCharFromString(szStr):
+	x = re.findall("[a-zA-Z]+", szStr)
+	i = 0
+	while i < len(x):
+		szStr = re.sub(x[i], "", szStr, 1)
+		i += 1
+	print("Removed all characters from string:{}".format(szStr))
+
+def main():
+	szEmailID = input("Enter e-mail ID:")
+	bRet = verifyemail(szEmailID)
+	if True == bRet:
+		print("Correct Email-ID")
+	else:
+		print("Incorrect Email-ID")
+		
+	rmCharFromString(szEmailID)
+if __name__ == '__main__':
+	main()
