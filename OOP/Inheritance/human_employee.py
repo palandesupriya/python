68fac11cfe69f0ba66f08ca355d7b28e49319e5a
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-03-10 18:18:50 +0530

    Create human_employee.py

---
+++ OOP/Inheritance/human_employee.py
@@ -0,0 +1,30 @@
+'''
+Implement inheritance of Human Employee.
+'''
+
+class Human(object):
+	def __init__(self, name, gender):
+		self.name = name
+		self.gender = gender
+		
+	def getDetails(self):
+		return self.name, self.gender
+		
+class Employee(Human):
+	def __init__(self, name, gender, experience, salary, desig):
+		Human.__init__(self, name, gender)
+		self.experience = experience
+		self.salary = salary
+		self.desig = desig
+		
+	def getDetails(self):
+		return Human.getDetails(self), self.experience,self.salary,self.desig
+	
+def main():
+	name,gender,experience,salary,desig = input("Enter name, gender, experience, salary, desig:")
+	emp = Employee(name,gender,experience,salary,desig)
+	#print emp.getDetails()
+	print emp.__dict__
+
+if __name__ == '__main__':
+	main()
