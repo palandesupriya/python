1128d6a525ead837dcc284833575f9296ca9d333
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-03-24 21:35:12 +0530

    Create filecnt_using_pipe.py

---
+++ subprocess/filecnt_using_pipe.py
@@ -0,0 +1,27 @@
+import subprocess
+def main():
+    fd = open("main.py", "r")
+    pipeHandle = subprocess.Popen("cat -", shell = True, stdout = subprocess.PIPE, stdin = fd)
+    stdin_val = pipeHandle.communicate()
+    fd.close()
+    iTemp = 0
+    iWordCnt = 0
+    iCharCnt = 0
+    iLineCnt = 1
+    while (iTemp < len(stdin_val)):
+        ch = stdin_val[iTemp]
+        if '\n' == ch:
+            iWordCnt += 1
+            iLineCnt += 1
+            iCharCnt += 1
+        elif ' ' == ch:
+            iWordCnt += 1
+            iCharCnt += 1
+        else:
+            iCharCnt += 1
+        iTemp += 1
+    print("Number of words:{}".format(iWordCnt))
+    print("Number of lines:{}".format(iLineCnt))
+    print("Number of characters:{}".format(iCharCnt))
+if __name__ == '__main__':
+    main()
