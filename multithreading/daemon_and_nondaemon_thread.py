60054bac1f7da883773144dc9b86463247860742
Supriya Palande <34849809+palandesupriya@users.noreply.github.com>
2018-04-07 14:30:50 +0530

    Create daemon_and_nondaemon_thread.py

---
+++ multithreading/daemon_and_nondaemon_thread.py
@@ -0,0 +1,21 @@
+import threading
+import time
+
+def daemon():
+    while True:
+        print("Going to sleep\n")
+        time.sleep(1)
+        print("Wakeup after sleep\n")
+
+def non_daemon():
+    time.sleep(5)
+    print("doing nothing\n")
+
+d = threading.Thread(name='daemon', target=daemon)
+d.setDaemon(True)
+t = threading.Thread(name='non-daemon', target=non_daemon)
+
+d.start()
+t.start()
+t.join(3)
+print t.isAlive()
